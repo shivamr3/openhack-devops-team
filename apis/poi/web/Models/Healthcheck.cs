86d434446181e406f912dc392f5a40ef377ae22d
DarinShapiroMS <dashapir@microsoft.com>
2020-09-16 08:43:54 -0700

    Update Healthcheck.cs
    updated to passing test value

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -8,7 +8,7 @@ namespace poi.Models
         public Healthcheck()
         {
             Message = "POI Service Healthcheck";
-            Status = "not as Healthy as I should be";
+            Status = "Healthy";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
