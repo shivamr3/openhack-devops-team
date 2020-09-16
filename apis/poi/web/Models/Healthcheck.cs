19879775212a4e485a225415de47323470ccd7ce
Allison Grimaldi <63111937+aagrimaldi@users.noreply.github.com>
2020-09-16 09:36:46 -0700

    Update Healthcheck.cs

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -8,7 +8,7 @@ namespace poi.Models
         public Healthcheck()
         {
             Message = "POI Service Healthcheck";
-            Status = "Healthygdsdsds2dgchange";
+            Status = "Healthygdsdsds2dgchange3";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
